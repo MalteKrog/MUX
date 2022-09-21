205133c689aac0822a05063655951c42188f76a8
MalteKrog <90311355+MalteKrog@users.noreply.github.com>
2022-09-21 12:49:55 +0200

    Add files via upload

---
+++ JS/galleri.js
@@ -0,0 +1,60 @@
+const animation = document.querySelectorAll('.animation') 
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show", entry.isIntersecting)
+    })
+}, {
+    threshold: 0.8,
+})
+
+animation.forEach(animation => {
+    observer.observe(animation)
+})
+
+
+// Animate images
+
+const floatup = document.querySelectorAll('.floatup-one') 
+const floatup2 = document.querySelectorAll('.floatup-two')
+const floatup3 = document.querySelectorAll('.floatup-three') 
+const floatup4 = document.querySelectorAll('.floatup-four')
+
+const observer2 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show", entry.isIntersecting)
+    })
+}, {
+    threshold: 0.0,
+    rootMargin: "600px"
+})
+
+floatup.forEach(floatup => {
+    observer2.observe(floatup)
+})
+
+floatup2.forEach(floatup2 => {
+    observer2.observe(floatup2)
+}) 
+
+floatup3.forEach(floatup3 => {
+    observer2.observe(floatup3)
+})
+
+floatup4.forEach(floatup4 => {
+    observer2.observe(floatup4)
+})
+
+// Audio
+
+var wasplayed = false
+  window.addEventListener("scroll", function() {
+    var scroll = this.scrollY;
+    if (scroll > 500 && scroll < 3000 && !wasplayed) {
+        var audio = new Audio('Media/Audio/Crowd.mp3');
+        audio.play();
+        audio.volume = 0.02;
+        audio.playbackRate = 2;
+        wasplayed = true;
+    }
+});
---
+++ JS/kontakt.js
@@ -0,0 +1,44 @@
+
+var distFromTop5 = document.querySelector(".card-container").offsetTop - 150;
+
+window.addEventListener("scroll", function() {
+    var scroll = this.scrollY;
+    if (scroll > distFromTop5) {
+        document.getElementById("mux-logo").style.filter = "invert(100%)";
+        document.querySelector(".findtickets-btn").style.color = "black";
+        document.querySelector(".findtickets-btn").style.border = "3px solid black";
+        document.querySelector(".span").classList.add('black');
+        document.querySelector(".span").style.backgroundColor = "black";
+    } else if (scroll < distFromTop5) {
+        document.getElementById("mux-logo").style.filter = "invert(0%)";
+        document.querySelector(".findtickets-btn").style.color = "white";
+        document.querySelector(".findtickets-btn").style.border = "3px solid white";
+        document.querySelector(".span").classList.remove('black');
+        document.querySelector(".span").style.backgroundColor = "white";
+    }
+})  
+
+const copy = document.querySelector(".copy");
+
+copy.onclick = function() {
+  document.execCommand("copy");
+}
+
+copy.addEventListener("copy", function(event) {
+  event.preventDefault();
+  if (event.clipboardData) {
+    event.clipboardData.setData("text/plain", copy.textContent);
+    console.log(event.clipboardData.getData("text"))
+  }
+});
+
+// Designet af
+const designetaf = document.querySelector('.designet-af-class').addEventListener('click', function() {
+    let divElement = document.querySelector(".card");
+    divElement.classList.remove("gem-popup");
+});
+
+const okay = document.querySelector('.button-primary').addEventListener('click', function() {
+    let divElement = document.querySelector(".card");
+    divElement.classList.add("gem-popup");
+});
